57e0af10467dd07112a74b127d6d172777cb851d
bertzhou <15157276075@163.com>
2021-04-07 22:09:38 +0800

    save

--- src/plugins/v-charts.js
+++
@@ -1,3 +0,0 @@
-import Vue from 'vue'
-import Vehist from 'v-charts/lib/histogram.common'
-Vue.component(Vehist.name,Vehist)
--- vue.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  outputDir: 'dist',
-  lintOnSave: false,
-  css: {
-    // 将组件内的 CSS 提取到一个单独的 CSS 文件 (只用在生产环境中)
-    extract: false,
-  }
-}
